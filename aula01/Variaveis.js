98cee15cc73d78ed97013f184436b585378c0b39
Rousingbeef1 <luccasgabriel35@gmail.com>
2024-06-07 21:57:39 -0300

    Update Variaveis.js
    Variáveis e template strings

---
+++ aula01/Variaveis.js
@@ -0,0 +1,15 @@
+const AmorDaMinhaVida = [{ nome: 'Laís', sobrenome: 'Rocha', idade: 18, filha: { nome: 'Dandara', } }];
+
+console.log(AmorDaMinhaVida)
+
+const palavra = 10
+const numero = 200
+
+console.log(typeof(palavra + numero))
+
+console.log(`O número 210 pode ser a soma de dois números ${palavra} e ${numero}`)
+
+const termo1 = 'variavel'
+const termo2 = `termo 2 é uma ${termo1}`
+
+console.log(termo2)
