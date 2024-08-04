fc990f9673150526166dcf7d32ecf0d97e0d9177
Rousingbeef1 <luccasgabriel35@gmail.com>
2024-08-04 16:26:35 -0300

    a
    aula 3 e 4

---
+++ aula03/Number.js
@@ -0,0 +1,8 @@
+/*Number é um objeto que dentre as funcionalidades pode converter strings para números*/
+
+let string = `2.7`
+let numero = 2
+console.log(typeof(Number(string)))       
+console.log(typeof(numero.toString()))      //passa para string
+
+console.log(typeof string);
---
+++ aula04/Short-circuit.js
@@ -0,0 +1,7 @@
+/*O curto circuito no javaScript executa um teste lógico com o operador &&, caso ele encontre um valor falso,
+ele é retornado, caso não, o último valor é retornado*/
+
+console.log(`luiz otávio` && true && `Maria`) 
+
+console.log(false || true) //O ou sempre retorna o primeiro true
+
